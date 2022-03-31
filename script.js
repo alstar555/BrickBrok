d628786aea8eab86ccebcc27705180ecfc1f33a3
Angelica <alstar555@gmail.com>
2022-03-31 14:03:22 -0400

    added second player

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var ctx = canvas.getContext("2d");
 //constants
 const BRICK_WIDTH = 60;
 const BRICK_HEIGHT = 20;
+const BRICK_X = 10;
+const BRICK_Y = 120;
 const BRICK_COLOR = "#53795C";
 const PLAYER_WIDTH = 60;
 const PLAYER_HEIGHT = 20;
 const PLAYER_COLOR = "#698377";
-const PLAYER_START_X = 430;
-const PLAYER_START_Y = 400;
+const PLAYER1_START_X = 430;
+const PLAYER1_START_Y = 400;
+const PLAYER2_START_X = 430;
+const PLAYER2_START_Y = 30;
 const BALL_RADIUS = 10;
 const BALL_CENTERX = 460;
 const BALL_CENTERY = 380;
@@ -21,20 +25,22 @@ var gameScore;
 var gameOver;
 var gameBrick;
 var gameBrickList = [];
-var player;
+var player1;
+var player2;
 var ball;
 
 
 startGame();
 
 function startGame(){
-    player = new Player(PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_COLOR, PLAYER_START_X, PLAYER_START_Y);
+    player1 = new Player(PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_COLOR, PLAYER1_START_X, PLAYER1_START_Y);
+    player2 = new Player(PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_COLOR, PLAYER2_START_X, PLAYER2_START_Y);
     ball = new Ball(BALL_RADIUS, BALL_CENTERX, BALL_CENTERY, BALL_COLOR);
 
     gameOver = false;
     gameScore = 0;
-    var x = 10;
-    var y = 10
+    var x = BRICK_X;
+    var y = BRICK_Y;
     for(let i = 0; i != 5; i++){
         for(let j = 0; j!=14; j++){
             gameBrick = new Brick(BRICK_WIDTH, BRICK_HEIGHT, BRICK_COLOR, x, y);
@@ -91,7 +97,8 @@ function drawBall(ball){
 
 function render(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPlayer(player);
+    drawPlayer(player1);
+    drawPlayer(player2);
     drawBall(ball);
     for(const brick of gameBrickList){
         drawBrick(brick);
@@ -117,10 +124,16 @@ document.body.onkeydown = function( e ){
 
 function movePlayer(dir){
     if(dir == "left"){
-        player.x -= 10;
+        player1.x -= 10;
     }
     else if(dir == "right"){
-        player.x += 10;
+        player1.x += 10;
+    }
+    else if(dir == "a"){
+        player2.x -= 10;
+    }
+    else if(dir == "d"){
+        player2.x += 10;
     }
     render();
 
